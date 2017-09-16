dc3a7ae7a61c273fbfea5c56b0c04268098bbb4f
Suhel Tanveer <suhel.tanveer@abof.adityabirla.com>
2017-09-16 14:18:34 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# webpack3
+webpack3 configuration
+
+- npm i or yarn
+- Run the required script mentioned in package.json
---
+++ src/index.js
@@ -0,0 +1,32 @@
+import _ from 'lodash'
+import printMe from './print.js';
+import './styles.css';
+
+function component() {
+  var element = document.createElement('div');
+  var btn = document.createElement('button');
+
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+  return element;
+}
+
+let element = component(); // Store the element to re-render on print.js changes
+document.body.appendChild(element);
+
+
+
+if (module.hot) {
+  module.hot.accept('./print.js', function() {
+    console.log('Accepting the updated printMe module!')
+    document.body.removeChild(element)
+    element = component() // Re-render the "component" to update the click handler
+    document.body.appendChild(element)
+  })
+}
