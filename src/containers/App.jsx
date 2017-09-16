c0e66c59fed1dc7655f1c8d0f591ab7e1abb8ba6
Suhel Tanveer <suhel.tanveer@abof.adityabirla.com>
2017-09-17 01:42:09 +0530

    Clock component added as an example

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,19 +1,21 @@
 import React, { Component } from 'react'
 import '../styles.scss'
 
+import Clock from './Clock.jsx'
+
 class App extends Component {
+
   render() {
     return (
       <div>
-        <header>Ensure Copy</header>
+        <header>
+          <h1>Page header</h1>
+        </header>
+        <Clock />
         <main>
           <div>
             <p>Page Content 1</p>
           </div>
-          <div>Page Content 2</div>
-          <div>Page Content 3</div>
-          <div>Page Content 4</div>
-          <div>Page Content 5</div>
         </main>
         <footer>Page footer</footer>
       </div>
